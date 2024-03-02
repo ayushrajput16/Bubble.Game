bd22e06d44d0d674378c10bde033cc6829fb828b
Ayush Kumar <145752926+ayushrajput16@users.noreply.github.com>
2024-03-02 13:06:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+
+var score = 0;
+var hitrn = 0;
+
+function increaseScore(){
+    score += 10;
+    document.querySelector("#scoreval") . textContent = score;
+}
+
+function makeBubble(){
+    var clutter = "";
+
+for(var i = 1; i<=75; i++){
+    rn = Math.floor(Math.random()*10);
+    clutter += `<div class="bubble">${rn}</div> `;
+}
+
+document.querySelector("#pbtm").innerHTML = clutter;
+}
+
+var timer = 60;
+function runTimer(){
+    var timerint = setInterval(function () {
+        if(timer > 0){
+            timer--;
+        document.querySelector("#timervalue").textContent = timer;
+        }
+        else{
+            clearInterval(timerint);
+            document.querySelector("#pbtm").innerHTML = `<h1>Game Over</h1>`;
+        }
+    }, 1000);  
+}
+
+function getNewHit(){
+hitrn = Math.floor(Math.random() * 10);
+    document.querySelector("#hitval"). textContent = hitrn;
+}
+
+document.querySelector("#pbtm")
+.addEventListener("click",function (dets) {
+    var clickednum = Number(dets.target.textContent);
+    if(clickednum === hitrn){
+        increaseScore();
+        makeBubble();
+        getNewHit();
+    }
+});
+
+makeBubble();
+runTimer();
+getNewHit();
+increaseScore();
